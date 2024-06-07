63f24d7d5c4777d8d0233745dcd66d7155ea1bfe
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-06-07 12:27:51 +0200

    RLE

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -3,8 +3,21 @@ package iut.sae.algo;
 
 public class Algo{
     public static String RLE(String in){
-        // Provide your algo here
-        return "NotYetImplemented";
+        String res = ""; 
+        int frequence = 0; 
+        char tab [] =  new char[in.length()]; 
+        int i;
+        for (i =0 ; i < in.length(); i++) {
+           tab[i]= in.charAt(i); 
+        }
+        for (i = 0; i< tab.length; i++){
+            if (tab[i]== tab[i+1]){
+            frequence ++; 
+            res = res + tab[i] + frequence; 
+           }
+        }
+        System.out.println(tab);
+        return res; 
     }
 
     public static String RLE(String in, int iteration) throws AlgoException{
