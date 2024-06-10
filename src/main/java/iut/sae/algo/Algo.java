f1ddc930849dabc92924f3ee09c5349db8398502
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-06-10 09:27:03 +0200

    AlgoRle

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -5,18 +5,29 @@ public class Algo{
     public static String RLE(String in){
         String res = ""; 
         int frequence = 0; 
-        char tab [] =  new char[in.length()]; 
+        /*char tab [] =  new char[in.length()+1]; 
         int i;
         for (i =0 ; i < in.length(); i++) {
            tab[i]= in.charAt(i); 
         }
         for (i = 0; i< tab.length; i++){
-            if (tab[i]== tab[i+1]){
-            frequence ++; 
-            res = res + tab[i] + frequence; 
-           }
+            while (tab[i] == tab[i+1]){
+                frequence ++; 
+            }
+            res = res + frequence +tab[i]; 
         }
         System.out.println(tab);
+        return res; */
+        int i = 0; 
+        int taille = in.length() +1 ; 
+        while(i < taille){ 
+            while (in.charAt(i) == in.charAt(i+1)) {
+                frequence ++;  
+                i++; 
+            }
+            res = res + frequence + in.charAt(i); 
+            i = frequence; 
+        }
         return res; 
     }
 
